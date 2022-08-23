08b4dcd321370cdf22298e6fd03927c99cabec99
ca1878 <72635836+ca1878@users.noreply.github.com>
2022-08-23 13:35:44 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,11 +5,25 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "mtls"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
+        answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "4"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "2"
+    else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         answer = ""
@@ -19,8 +33,25 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
+    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+    print(welcome_assignment_answers(debug_question))
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
+    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is it possible to decode a message without a key? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
+    print(welcome_assignment_answers(debug_question))
+
 
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
@@ -30,5 +61,5 @@ if __name__ == "__main__":
 #"Is a hashed message supposed to be un-hashed? - Yes/No":
 #"What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the OSI model does the protocol DHCP belong to? - The answer should be an integer number":
-#"What layer of the OSI model does the protocol TCP belong to? - The answer should be an integer number":
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
